244a6d7e389d773bc76fd0f61cee48cdb2412ffd
Chase MP <rush@deviantart.com>
2013-09-05 14:37:27 -0700

    ditch .profile

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+rm -f ~/.bashrc
+mv ~/.profile ~./bakprofile
+rm -f ~/.bash_profile
 ln -s ~/settings/git/gitconfig ~/.gitconfig
 ln -s ~/settings/bash/bash_profile ~/.bash_profile
-rm -f ~/.bashrc
 ln -s ~/settings/bash/bashrc ~/.bashrc
