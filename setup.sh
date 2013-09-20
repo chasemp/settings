113e83910982cb82b606e4adac766ce60afb08f6
Chase MP <rush@deviantart.com>
2013-09-20 16:36:06 -0700

    vimrc

--- setup.sh
+++ setup.sh
@@ -10,7 +10,9 @@ rmfile ~/.bashrc
 rmfile ~/.bash_profile
 rmfile ~/.gitconfig
 rmfile ~/.sup.ini
+rmfile ~/.vimrc
 ln -s ~/settings/git/gitconfig ~/.gitconfig
 ln -s ~/settings/bash/bash_profile ~/.bash_profile
 ln -s ~/settings/bash/bashrc ~/.bashrc
 ln -s ~/settings/sup/sup.ini ~/.sup.ini
+ln -s ~/settings/vim/vimrc ~/.vimrc
