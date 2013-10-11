0bcbbfdd7a627572b199a152cc0bb77232177bd7
Chase MP <rush@deviantart.com>
2013-10-11 13:57:22 -0700

    reset bin stuff

--- setup.sh
+++ setup.sh
@@ -10,13 +10,14 @@ rmfile ~/.bash_profile
 rmfile ~/.gitconfig
 rmfile ~/.sup.ini
 rmfile ~/.vimrc
+
 ln -s "$PWD/git/gitconfig" ~/.gitconfig
 ln -s "$PWD/bash/bash_profile" ~/.bash_profile
 ln -s "$PWD/bash/bashrc" ~/.bashrc
 ln -s "$PWD/sup/sup.ini" ~/.sup.ini
 ln -s "$PWD/vim/vimrc" ~/.vimrc
 
-chmod +x $PWD/bin/*
-rm -f /usr/local/bin/prompt.sh
-ln -s ~/settings/bin/prompt.sh /usr/local/bin/prompt.sh
+rm -f ~/bin/prompt.sh
+ln -s ~/settings/bin/prompt.sh ~/bin/prompt.sh
+chmod +x ~/bin/*
 
