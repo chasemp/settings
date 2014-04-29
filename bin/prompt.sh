9096d5d6f6161f987a00bf69797f6d6933a72a30
Chase Pettet [rush] <chase.mp@gmail.com>
2014-04-29 11:28:24 -0500

    prompt colors I will deal with you later

--- bin/prompt.sh
+++ bin/prompt.sh
@@ -1,21 +1,3 @@
-NORMAL=$(tput sgr0)
-RED=$(tput setaf 1; tput bold)
-MAGENTA=$(tput setaf 5)
-BLUE=$(tput setaf 4)
-
-function red() {
-    echo -e "$RED$*$NORMAL"
-}
-
-function mag() {
-    echo -e "$MAGENTA$*$NORMAL"
-}
-
-function blue() {
-    echo -e "$BLUE$*$NORMAL"
-}
-
-
 function parse_git_branch {
     gs=$(git rev-parse --symbolic-full-name --abbrev-ref HEAD 2> /dev/null)
     if [ -z "$gs" ]; then
@@ -23,12 +5,9 @@ function parse_git_branch {
     fi
     if [ "$gs" == "master" ]
     then
-        gs=$(mag "m")
+        echo "[m]"
     else
-        gs=$(red "$gs")
+        echo "[$gs]"
     fi
-
-    echo "[$gs]"
 }
-
 parse_git_branch
